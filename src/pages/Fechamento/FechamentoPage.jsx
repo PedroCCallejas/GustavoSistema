e4b41da3ec8d1a79f2cbe7cf9bd0fd95c5e0f2b6
PedroCCallejas <pedroccallejas@hotmail.com>
2026-07-18 12:58:49 -0400

    medico veterinario

--- src/pages/Fechamento/FechamentoPage.jsx
+++ src/pages/Fechamento/FechamentoPage.jsx
@@ -314,7 +314,7 @@ export default function FechamentoPage() {
             <HeaderFechamento
               logo={logo}
               nome="Gustavo Miguel Monteiro de Andrade"
-              subtitulo="Clínica & Podologia Equina"
+              subtitulo="Médico Veterinário"
               crmv="CRMV-MT 08415"
               tituloDocumento="Fechamento de Conta"
               data={client.date}
